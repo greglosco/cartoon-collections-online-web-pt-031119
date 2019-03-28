fe672e2117cd1df8d0cdfbe79ef0e41d9a266ae3
Gregory Losco <gtlosco@gmail.com>
2019-03-28 14:03:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index {|dwarf, i| puts "#{i+1}. #{dwarf}"}
 end
@@ -7,10 +9,28 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(calls)
-  calls.any? {|i| i.length > 4 }
+ calls.any? {|i| i.length > 4}
 end
 
 def find_the_cheese(array)
-  cheeses = %w[gouda cheddar camembert]
-  array.find {|i| cheeses.include?(i)}
+  types_of_cheese = ["cheddar", "gouda", "camembert"]
+  
+  array.find {|cheese| types_of_cheese.include?(cheese)}
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def long_planeteer_calls(calls)
+  calls.any? {|i| i.length > 4 }
 end
